51df671af9e6cc2a4021debe5a5faccce25d63bd
Ivan <ivan.maliyov@gmail.com>
2022-03-02 18:52:52 -0800

    example files added

---
+++ example04-gaas-polar/pw-ph-wann/phonon/ph-submit.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+PREFIX='gaas'
+
+#mode='gamma'
+
+SUBJOB='qe-tempo-ph-v64.pbs'
+
+HERE=`pwd`
+for ((NQ=1; NQ<=29; NQ++ ))
+#for ((NQ=2; NQ<=29; NQ++ ))
+do
+   DIR=ph-$NQ
+   echo $DIR
+   mkdir -p $DIR
+   
+   cd $DIR
+   #change ph.in
+   cp ../ph-ref.in  ./ph.in
+   sed -i   "s|.*prefix.*|  prefix='${PREFIX}'|g"      ph.in
+   sed -i   "s|.*fildyn.*|  fildyn='${PREFIX}.dyn.xml'|g"  ph.in
+   sed -i  "s|.*start_q.*|  start_q= ${NQ}|g"  ph.in
+   sed -i   "s|.*last_q.*|  last_q = ${NQ}|g"  ph.in
+   #change submission script
+   cp ../${SUBJOB} ./
+   sed -i  "s|#PBS -N pert-ph|#PBS -N ${PREFIX}-ph-q${NQ}|g" ${SUBJOB}
+   sed -i  "s|.*test-ph.*|#PBS -N ${PREFIX}-ph-q${NQ}|g" ${SUBJOB}
+   sed -i  "s|.*NQ=.*|NQ=${NQ}|g" ${SUBJOB}
+   sed -i  "s|.*PREFIX=.*|PREFIX=${PREFIX}|g" ${SUBJOB}
+   # different dir in q=0 calculation.
+   if [ "$NQ" == "1" ]; then
+      sed -i "s|.*DPATH=.*| DPATH='.' |g"  ${SUBJOB}
+   fi
+   # submit 
+   qsub ${SUBJOB}
+   echo Done $DIR
+   # wait for a short period of time.
+   #done, return to upper directory.
+   cd ..
+   
+   # for gamma point only
+   if [ "$NQ" == "1" ] && [ "$mode" == "gamma" ]; then
+      break
+   fi
+
+   sleep 1
+done
---
+++ example05-graphene-2d/pw-ph-wann/phonon/ph-collect.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PREFIX='graphene'
+
+#should be in the work directory of PHonon calculation
+echo `pwd`
+mkdir -p save 
+mkdir -p save/${PREFIX}.phsave
+
+for ((NQ=1; NQ<=37; NQ++ ))
+do
+   DIR="ph-$NQ/tmp/_ph0"
+   echo $DIR
+   #copy prefix.phsave
+   cp ${DIR}/${PREFIX}.phsave/* save/${PREFIX}.phsave/
+   #copy dyn files
+   #cp ph-${NQ}/${PREFIX}.dyn${NQ} save/  #${PREFIX}.dyn_q${NQ}
+   cp ph-${NQ}/${PREFIX}.dyn* save/
+   #copy dvscf files
+   cp ${DIR}/${PREFIX}.q_${NQ}/${PREFIX}.dvscf1 save/${PREFIX}.dvscf_q${NQ}
+done
